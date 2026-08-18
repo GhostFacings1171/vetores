7ba0e7b24bbadde9ababe00b0a75129ad745af70
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-17 22:46:28 -0300

    ex7 final

--- src/ex7.java
+++ src/ex7.java
@@ -3,11 +3,25 @@ import java.util.Random;
 public class ex7 {
     static void main() {
         Random random = new Random();
-        int[] x = new int[10];
+        int[] x = new int[5];
+        int cont, grau = 0;
 
         for (int i = 0; i < x.length; i++) {
             x[i] = random.nextInt(1, 10);
-            System.out.println(x[i] + "\t");
+            System.out.print(x[i] + "\t");
         }
+
+        for (int i = 0; i < x.length; i++) {
+            cont = 0;
+            for (int j = i; j < x.length; j++) {
+                if (x[i] == x[j]){
+                    cont++;
+                }
+            }
+            if (cont > grau){
+                grau = cont;
+            }
+        }
+        System.out.println("\n" + grau);
     }
 }
